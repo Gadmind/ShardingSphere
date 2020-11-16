e084aa2a709854625af42aa2618250995c0596c0
Gadmind <admin@gadmind.com>
2020-11-16 23:47:26 +0800

    Mapper映射----公共字典ORM

---
+++ src/main/java/com/daop/shardingsphere/mapper/DictMapper.java
@@ -0,0 +1,16 @@
+package com.daop.shardingsphere.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.daop.shardingsphere.entity.Dict;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @BelongsProject: ShardingSphere
+ * @BelongsPackage: com.daop.shardingsphere.mapper
+ * @Description:
+ * @DATE: 2020-11-16 23:13
+ * @AUTHOR: Daop
+ **/
+@Repository
+public interface DictMapper extends BaseMapper<Dict> {
+}
