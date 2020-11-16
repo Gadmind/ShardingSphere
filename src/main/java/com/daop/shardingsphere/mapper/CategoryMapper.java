de2dbc32429bee90b7a182e1c50eb233d2bd2139
Gadmind <admin@gadmind.com>
2020-11-16 23:47:11 +0800

    Mapper映射----分类ORM

---
+++ src/main/java/com/daop/shardingsphere/mapper/CategoryMapper.java
@@ -0,0 +1,16 @@
+package com.daop.shardingsphere.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.daop.shardingsphere.entity.Category;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @BelongsProject: ShardingSphere
+ * @BelongsPackage: com.daop.shardingsphere.mapper
+ * @Description:
+ * @DATE: 2020-11-16 22:32
+ * @AUTHOR: Daop
+ **/
+@Repository
+public interface CategoryMapper extends BaseMapper<Category> {
+}
